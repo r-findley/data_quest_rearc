48edc08a6c899d483af9b11dae206e5604e0fde4
Ryan Findley <ryangoesrunning@gmail.com>
2025-08-29 19:31:28 -0700

    Added lambda_two

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,11 +30,11 @@ resource "aws_s3_bucket" "s3_bucket" {
     Name        = "rearc-data-quest"
     Environment = "Dev"
   }
-  }
+}
 
 resource "aws_s3_bucket_policy" "allow_jupyter_user_read" {
   provider = aws.sandbox
-  bucket = aws_s3_bucket.s3_bucket.id
+  bucket   = aws_s3_bucket.s3_bucket.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -43,7 +43,7 @@ resource "aws_s3_bucket_policy" "allow_jupyter_user_read" {
         Principal = {
           AWS = "arn:aws:iam::542403648992:user/jupyter_user"
         },
-        Action = ["s3:GetObject"],
+        Action   = ["s3:GetObject"],
         Resource = "${aws_s3_bucket.s3_bucket.arn}/*"
       }
     ]
@@ -58,6 +58,7 @@ resource "aws_sqs_queue" "sqs_queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
+  visibility_timeout_seconds = 60
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
     maxReceiveCount     = 4
@@ -110,6 +111,16 @@ data "archive_file" "lambda_one_payload" {
   output_path = "${var.lambda_one_src_dir}/payload.zip"
 }
 
+data "archive_file" "lambda_two_payload" {
+  type       = "zip"
+  source_dir = var.lambda_two_src_dir
+  excludes = [
+    "venv",
+    "__pycache__"
+  ]
+  output_path = "${var.lambda_two_src_dir}/payload.zip"
+}
+
 resource "aws_iam_role" "lambda_exec" {
   provider = aws.sandbox
   name     = "lambda_exec_role"
@@ -125,19 +136,20 @@ resource "aws_iam_role" "lambda_exec" {
     }]
   })
 }
-  resource "aws_iam_role_policy_attachment" "lambda_exec_basic" {
-    provider   = aws.sandbox
-    role       = aws_iam_role.lambda_exec.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  }
 
-  resource "aws_lambda_layer_version" "lambda_one_dependencies" {
-    provider            = aws.sandbox
-    filename            = "${path.module}/../layer.zip"
-    layer_name          = "lambda_one_dependencies"
-    compatible_runtimes = ["python3.12"]
-    description         = "Dependencies for lambda_one"
-  }
+resource "aws_iam_role_policy_attachment" "lambda_exec_basic" {
+  provider   = aws.sandbox
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_layer_version" "shared_dependencies" {
+  provider            = aws.sandbox
+  filename            = "${path.module}/../layer.zip"
+  layer_name          = "shared_dependencies"
+  compatible_runtimes = ["python3.12"]
+  description         = "Shared dependencies for both lambdas"
+}
 
 resource "aws_lambda_function" "lambda_one" {
   provider      = aws.sandbox
@@ -150,11 +162,32 @@ resource "aws_lambda_function" "lambda_one" {
   filename         = data.archive_file.lambda_one_payload.output_path
   source_code_hash = data.archive_file.lambda_one_payload.output_base64sha256
 
-    layers = [aws_lambda_layer_version.lambda_one_dependencies.arn]
+  layers = [aws_lambda_layer_version.shared_dependencies.arn]
 
   environment {
     variables = {
-      BUCKET_NAME = aws_s3_bucket.s3_bucket.id
+      BUCKET_NAME   = aws_s3_bucket.s3_bucket.id
+      SQS_QUEUE_URL = aws_sqs_queue.sqs_queue.id
+    }
+  }
+}
+
+resource "aws_lambda_function" "lambda_two" {
+  provider      = aws.sandbox
+  function_name = "lambda_two"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "lambda_function.lambda_two_handler"
+  runtime       = "python3.12"
+  timeout       = 60
+
+  filename         = "${path.module}/../lambda_two/zip.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda_two/zip.zip")
+
+  layers = [aws_lambda_layer_version.shared_dependencies.arn]
+
+  environment {
+    variables = {
+      BUCKET_NAME   = aws_s3_bucket.s3_bucket.id
       SQS_QUEUE_URL = aws_sqs_queue.sqs_queue.id
     }
   }
@@ -204,6 +237,35 @@ resource "aws_iam_role_policy" "lambda_s3_access" {
           "${aws_s3_bucket.s3_bucket.arn}",
           "${aws_s3_bucket.s3_bucket.arn}/*"
         ]
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_mapping" {
+  provider         = aws.sandbox
+  event_source_arn = aws_sqs_queue.sqs_queue.arn
+  function_name    = aws_lambda_function.lambda_two.arn
+  batch_size       = 10
+  enabled          = true
+}
+
+resource "aws_iam_role_policy" "lambda_sqs_access" {
+  provider = aws.sandbox
+  name     = "lambda_sqs_access"
+  role     = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ],
+        Resource = aws_sqs_queue.sqs_queue.arn
       }
     ]
   })
---
+++ lambda_two/lambda_function.py
@@ -0,0 +1,97 @@
+import logging
+import os
+import boto3
+import pandas as pd
+import io
+import json
+from helpers import (
+    get_file_from_bucket,
+    trim_whitespace_from_df,
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_two_handler(event, context):
+    for record in event.get("Records", []):
+        logger.info(f"Lambda two triggered by {record}")
+    bucket_name = os.environ["BUCKET_NAME"]
+    s3_client = boto3.client("s3")
+    bls_file = "bls_data/pr.data.0.Current"
+    population_data = "datausa/datausa_population.json"
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_columns", None)
+    try:
+        bls_csv = get_file_from_bucket(
+            s3_client=s3_client, bucket_name=bucket_name, key=bls_file
+        )
+        logger.info("Retrieved BLS data from S3")
+        population_json = get_file_from_bucket(
+            s3_client=s3_client, bucket_name=bucket_name, key=population_data
+        )
+    except Exception as e:
+        logger.error(f"Unable to retrieve files from S3 - {e}", exc_info=True)
+
+    if bls_csv:
+        logger.info(f"Type of bls_csv: {type(bls_csv)}")
+        bls_df = pd.read_csv(io.StringIO(bls_csv), sep="\t")
+        bls_df = trim_whitespace_from_df(bls_df)
+        bls_df.columns = bls_df.columns.str.capitalize()
+        logger.info(f"BLS DataFrame loaded with {len(bls_df)} records")
+
+    if population_json:
+        logger.info(f"Type of population_json: {type(population_json)}")
+        population_data = json.loads(population_json)
+        population_df = pd.json_normalize(population_data, record_path=["data"])
+        population_df = trim_whitespace_from_df(population_df)
+        population_df["Population"] = population_df["Population"].astype(int)
+
+    try:
+        filtered_population_df = population_df[
+            (population_df["Year"] >= 2013) & (population_df["Year"] <= 2018)
+        ]
+        mean_population = filtered_population_df["Population"].mean()
+        standard_deviation = filtered_population_df["Population"].std()
+        logger.info(f"Mean Population (2013-2018): {int(mean_population):,}")
+        logger.info(f"Standard Deviation (2013-2018): {int(standard_deviation):,}")
+    except Exception as e:
+        logger.error(f"Error calculating population statistics - {e}", exc_info=True)
+
+    try:
+        series_grouped = (
+            bls_df.groupby(["Series_id", "Year"])["Value"].sum().reset_index()
+        )
+        best_years_idx = series_grouped.groupby("Series_id")["Value"].idxmax()
+        best_years_for_each_series = series_grouped.loc[
+            best_years_idx, ["Series_id", "Year", "Value"]
+        ]
+        logger.info(
+            f"Best years for each series calculated, total records: {best_years_for_each_series=})"
+        )
+    except Exception as e:
+        logger.error(
+            f"Error determining best years for each series - {e}", exc_info=True
+        )
+
+    try:
+        series_PRS30006032_data = bls_df.loc[
+            (bls_df["Series_id"] == "PRS30006032") & (bls_df["Period"] == "Q01")
+        ]
+        concatenated_df = series_PRS30006032_data.merge(
+            population_df, on="Year", how="inner"
+        )
+        concatenated_df = concatenated_df.drop(
+            columns=["Footnote_codes", "Nation ID", "Nation"]
+        )
+        concatenated_df = concatenated_df.rename(
+            columns={
+                "Value": "value",
+                "Year": "year",
+                "Series_id": "series",
+                "Period": "period",
+            }
+        )
+        logger.info(f"Concatenated Population and Series Dataframe: {concatenated_df=}")
+    except Exception as e:
+        logger.error(f"Error merging population and series data - {e}", exc_info=True)
